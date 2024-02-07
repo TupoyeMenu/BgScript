c4d72e90006d927515b85eaa708f81add3288ffb
updater <>
2024-02-07 08:14:29 +0100

    addded a shell script to run every hour

--- check_for_updates.py
+++ check_for_updates.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import os, hashlib
 import urllib.request
 
---
+++ persist.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+while true; do
+	echo "Running script."
+	./check_for_updates.py;
+	echo "Script ran."
+	sleep 60;
+done
